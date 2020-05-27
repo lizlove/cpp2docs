4f7a36be82c83af4c4afc58a5e45a17a338f32f1
lizlove <elizabethlovero@gmail.com>
2020-05-27 15:48:34 -0400

    Add includes and excluded items.

---
+++ src/test_ns.hpp
@@ -0,0 +1,19 @@
+
+#include <complex>
+#include <string>
+#include <vector>
+
+#define NO_INCLUDE
+
+#include "./test.hpp"
+
+namespace N1 {
+
+#include "./test.hpp"
+
+namespace N2 {
+#include "./test.hpp"
+
+}
+
+} // namespace N1
